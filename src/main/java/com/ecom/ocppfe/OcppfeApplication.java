0bc8c122764a171551917eaeeb1c29c9ec81027d
ayoub mouradi <ayoubmouradi70@gmail.com>
2025-11-07 13:46:07 +0100

    update mon PFE

---
+++ src/main/java/com/ecom/ocppfe/OcppfeApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.ocppfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcppfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcppfeApplication.class, args);
+    }
+
+}
